70480e82d0e8a8e9933a741c875af3238d0773b3
ML <valentin.medard.mlpm@outlook.fr>
2026-03-17 11:52:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,12 @@
 let isPinching = false;
 
-// Détecte pinch
+// Détecte le pinch à 2 doigts
 document.addEventListener('touchstart', e => {
     if (e.touches.length > 1) isPinching = true;
 }, { passive: false });
 
 document.addEventListener('touchend', e => {
     if (e.touches.length < 2) isPinching = false;
-}, { passive: false });
-
-// Bloque le flip seulement pendant le pinch
-pageFlip.on('flipStart', e => {
-    if (isPinching) {
-        e.preventDefault();  // empêche le flip
-    }
-});
-
-// Autorise le déplacement / drag après zoom
-document.getElementById('book').addEventListener('touchmove', e => {
-    if (isPinching) {
-        e.stopPropagation();   // empêche PageFlip de réagir
-        // ne bloque pas le scroll / drag du navigateur
-    }
 }, { passive: false });
 
 // Initialisation PageFlip
