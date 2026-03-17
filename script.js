d959f83109be4c7ba5edc4f6957d54d9200d1c38
ML <valentin.medard.mlpm@outlook.fr>
2026-03-17 10:59:09 +0100

    Refactor touch event handling for zoom functionality

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
+const bookEl = document.getElementById("book");
 let isZooming = false;
 
-// Détecte si l'utilisateur utilise 2 doigts → active le mode zoom
-document.addEventListener("touchstart", function(e) {
+// Initialisation PageFlip
+const pageFlip = new St.PageFlip(bookEl, {
+    width: 300,
+    height: 500,
+    size: "stretch",
+    showCover: true,
+    usePortrait: true,
+    mobileScrollSupport: true,
+    minWidth: 300,
+    maxWidth: 500,
+    minHeight: 400,
+    maxHeight: 700,
+    zoomEnabled: true,
+    pinchZoom: true,
+    maxZoom: 4
+});
+
+pageFlip.loadFromHTML(document.querySelectorAll(".my-page"));
+
+// Détecte si l'utilisateur utilise 2 doigts → mode zoom
+bookEl.addEventListener("touchstart", (e) => {
     if (e.touches.length > 1) {
         isZooming = true;
     }
 }, { passive: false });
 
-// Désactive le mode zoom quand il n'y a plus que 1 doigt
-document.addEventListener("touchend", function(e) {
+bookEl.addEventListener("touchend", (e) => {
     if (e.touches.length < 2) {
         isZooming = false;
     }
 }, { passive: false });
 
-// Empêche le flip PageFlip pendant le zoom
-document.addEventListener("touchmove", function(e) {
-    if (isZooming) {
+// Bloque le flip si zoom actif ou si le zoom est supérieur à 1
+bookEl.addEventListener("touchmove", (e) => {
+    const zoom = pageFlip.getZoom(); // récupère le niveau de zoom actuel
+    if (isZooming || zoom !== 1) {
         e.stopImmediatePropagation(); // bloque le flip
     }
 }, { passive: false });
-
-// Initialisation PageFlip
-const pageFlip = new St.PageFlip(document.getElementById("book"), {
-    width: 300,            
-    height: 500,
-    size: "stretch",        
-    showCover: true,
-    usePortrait: true,      
-    mobileScrollSupport: true, 
-    minWidth: 300,
-    maxWidth: 500,
-    minHeight: 400,
-    maxHeight: 700,
-
-    zoomEnabled: true,
-    pinchZoom: true,   
-    maxZoom: 4
-});
-
-pageFlip.loadFromHTML(document.querySelectorAll(".my-page"));
